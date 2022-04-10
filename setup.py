fe82d540708901c3168ced8deb754c7a129201ee
Nikola Jevtic <jevtic@Nikolas-MacBook-Air.local>
2022-04-10 21:54:09 +0200

    dodao include u find_packages

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='redshift_connector',
-    packages=find_packages(),
+    packages=find_packages(include=['redshift_connector']),
     version='0.1.0',
     description='Connector to redshift db',
     author='Nikola Jevtic',
